b07db80366bbc2eaab767c72caf368f24ea301d5
Cassin01 <koyojb@gmail.com>
2024-11-03 20:34:08 +0900

    add: add depend

---
+++ command/infra/depend.go
@@ -0,0 +1,16 @@
+package sqlboiler
+
+import (
+	"commandserivce/infra/sqlboiler/repository"
+
+	"go.uber.org/fx"
+)
+
+// SQLBoilerを利用したRepositoryの依存定義
+var RepDepend = fx.Options(
+	fx.Provide(
+		// Repositoryインターフェイス実装のコンストラクタを指定
+		repository.NewcategoryRepositorySQLBoiler, // カテゴリ用Repository
+		repository.NewproductRepsitorySQLBoiler,   // 商品用Repository
+	),
+)
